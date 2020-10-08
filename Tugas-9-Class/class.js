49000f3acb4dad297d901a547701e7dab77b1d62
Andika Haris <andikaharis68@gmail.com>
2020-10-08 16:45:02 +0700

    tugas 9 done

---
+++ Tugas-9-Class/class.js
@@ -0,0 +1,75 @@
+class Animal {
+    constructor(name) {
+        this.name = name
+        this.legs = 4
+        this.cold_blooded = false
+    }
+}
+class Ape extends Animal {
+    constructor(name) {
+        super(name)
+        this.legs = 2
+    }
+    yell() {
+        console.log("Auooo")
+    }
+}
+class Frog extends Animal {
+    constructor(name) {
+        super(name)
+    }
+    jump() {
+        console.log("hop hop")
+    }
+}
+
+var sheep = new Animal("shaun")
+console.log(sheep.name)
+console.log(sheep.legs)
+console.log(sheep.cold_blooded)
+
+var sungokong = new Ape("kera sakti")
+console.log(sungokong.name)
+console.log(sungokong.legs)
+console.log(sungokong.cold_blooded)
+sungokong.yell() // "Auooo"
+
+var kodok = new Frog("buduk")
+console.log(kodok.name)
+console.log(kodok.legs)
+console.log(kodok.cold_blooded)
+kodok.jump() // "hop hop" 
+
+
+
+class Clock {
+    constructor({ template }) {
+        this.template = template
+    }
+    render() {
+        var date = new Date();
+        var hours = date.getHours();
+        if (hours < 10) hours = '0' + hours;
+        var mins = date.getMinutes();
+        if (mins < 10) mins = '0' + mins;
+        var secs = date.getSeconds();
+        if (secs < 10) secs = '0' + secs;
+        var output = this.template
+            .replace('h', hours)
+            .replace('m', mins)
+            .replace('s', secs);
+        console.log(output);
+    }
+
+    stop() {
+        clearInterval(timer);
+    };
+
+    start() {
+        this.render();
+        this.timer = setInterval(this.render.bind(this), 1000);
+    };
+}
+
+var clock = new Clock({ template: 'h:m:s' });
+clock.start(); 
